629d146d14779a92ce1f0fc91941895cf6ca3431
sonukuldeep <sonukuldeep@outlook.com>
2023-04-24 09:51:24 +0530

    update 1.13

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // Serve static files from a public folder
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api/run', routes)
 /*
