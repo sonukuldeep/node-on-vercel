e8bb52c76e953b4e2d74dd155ff24f2c516b0b07
sonukuldeep <sonukuldeep@outlook.com>
2023-04-25 11:37:11 +0530

    update 2.14

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-"use strict";
+//Imports
 require('dotenv').config();
 var express = require("express");
 var app = express();
-var routes = require('./app/routes');
+var fitness = require('./app/fitness');
 var path = require('path');
+//Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Set the view engine to EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// Serve static files from a public folder
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes);
+// Routes
+app.use('/fitness', fitness);
+// if port exist then run else deploy on vercel
 if (process.env.PORT) {
+    // Start the server
     app.listen(process.env.PORT, function () {
         console.log("Server is running on port ".concat(process.env.PORT));
     });
