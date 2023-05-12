03948e7c296a6fe45c1d6c4213cacd78c1fd7321
sonukuldeep <sonukuldeep@outlook.com>
2023-05-12 23:39:00 +0530

    update

--- public/noize/js/script.js
+++ public/noize/js/script.js
@@ -1,56 +1,24 @@
-function buildThresholdList(numOfSteps) {
-    let thresholds = [];
-  
-    for (let i = 1.0; i <= numOfSteps; i++) {
-      let ratio = i / numOfSteps;
-      thresholds.push(ratio);
-    }
-  
-    thresholds.push(0);
-    return thresholds;
-  }
 
-  ANIMATION_CLASS='.animate__animated'
-  ANIMATION_TYPE='animate__fadeInUpBig'
-  
-  // getting cards to listen to
-  const cards = document.querySelectorAll(ANIMATION_CLASS);
-  
-  // Set things up
-  window.addEventListener(
-    "load",
-    event => {
-      createObserver();
-    },
-    false
-  );
-  
-  function createObserver() {
-    let observer;
-  
-    let options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
-    };
-  
-    observer = new IntersectionObserver(handleIntersect, options);
-  
-    // pass objects to observe
-    cards.forEach(card => {
-      observer.observe(card);
-    });
+ANIMATION_CLASS = '.animate__animated'
+ANIMATION_TYPE = 'animate__fadeInUpBig'
+const cards = document.querySelectorAll(ANIMATION_CLASS)
+const windowHeight = window.innerHeight; // gets viewport height
+
+
+function animateFunction(element, delay = 1) {
+  const top = element.getBoundingClientRect().top; // gets element top relative to view port
+  const topFactor = delay; // lower value to delay animation and vice versa
+  const range = topFactor * windowHeight - top; // if window - top is + then object is in view
+
+  if (range > 0) {
+    element.classList.add(ANIMATION_TYPE)
+
   }
-  
-  function handleIntersect(entries, observer) {
-    entries.forEach(entry => {
-      // the condition that needs changing
-      entry.target.classList.toggle(ANIMATION_TYPE, entry.isIntersecting);
-    //   entry.target.style.opacity = entry.intersectionRatio.toFixed(2);
-      console.log(entry.intersectionRatio.toFixed(2));
-  
-    //   run once
-      if (entry.isIntersecting)
-          observer.unobserve(entry.target)
-    });
-  }
+}
+
+window.addEventListener(
+  "scroll",
+  () => {
+    cards.forEach((card, index) => { if (index !== 0) animateFunction(card) })
+  },
+);
