8da2906dd437858698cdae4e3ae11ded6ade259e
sonukuldeep <sonukuldeep@outlook.com>
2023-09-16 17:25:42 +0530

    update on influencer

--- app/influencer.js
+++ app/influencer.js
@@ -8,19 +8,11 @@ router.get("/test", function (req, res) {
     res.status(200).send("works");
 });
 router.get("/", function (req, res) {
-    res.status(200).render("influencer/index");
+    res.status(200).render("influencer/index", { path: process.env.INFLUENCER });
 });
 router.post("/", function (req, res) {
     var _a = req.body, num1 = _a.num1, num2 = _a.num2;
     res.status(200).send("The sum of these numbers is " + num1 + num2);
-});
-router.get("/*", function (req, res) {
-    res
-        .status(200)
-        .render("noize/404", {
-        path: process.env.NOIZE_PATH,
-        imgUrl: process.env.NOIZE_IMAGE_PATH,
-    });
 });
 // Export the router
 module.exports = router;
--- app/influencer.ts
+++ app/influencer.ts
@@ -8,21 +8,12 @@ router.get("/test", function (req: Request, res: Response) {
 });
 
 router.get("/", function (req: Request, res: Response) {
-  res.status(200).render("influencer/index");
+  res.status(200).render("influencer/index", { path: process.env.INFLUENCER });
 });
 
 router.post("/", function (req: Request, res: Response) {
   const { num1, num2 } = req.body;
   res.status(200).send("The sum of these numbers is " + num1 + num2);
-});
-
-router.get("/*", function (req: Request, res: Response) {
-  res
-    .status(200)
-    .render("noize/404", {
-      path: process.env.NOIZE_PATH,
-      imgUrl: process.env.NOIZE_IMAGE_PATH,
-    });
 });
 
 // Export the router
